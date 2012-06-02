bf8c3d9cceb9fb795dc27fbe643e62fddff9eeee
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-02 19:29:31 +0200

    Init my arduino project with clojure :D

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject arduino-lab "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clodiuno "0.0.3-SNAPSHOT"]]
+  :native-dependencies [[org.clojars.samaaron/rxtx "2.2.0.1"]]
+  :dev-dependencies [[native-deps "1.0.5"]]
+  :jvm-opts ["-d32"])
+
---
+++ test/arduino_lab/test/core.clj
@@ -0,0 +1,6 @@
+(ns arduino-lab.test.core
+  (:use [arduino-lab.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
