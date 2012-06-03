579becdeeb1a9c391d00daad291499d6b58450dc
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-03 22:25:53 +0200

    More clear name.

--- src/arduino_lab/morse.clj
+++ src/arduino_lab/morse.clj
@@ -6,32 +6,32 @@
 ;; 0 represents short signal
 ;; 1 represents long signal
 ;; Intermational morse from http://en.wikipedia.org/wiki/Morse_code
-(def morse {\a [0 1]
-            \b [1 0 0 0]
-            \c [1 0 1 0]
-            \d [1 0 0]
-            \e [0]
-            \f [0 0 1 0]
-            \g [1 1 0]
-            \h [0 0 0 0]
-            \i [0 0]
-            \j [0 1 1 1]
-            \k [1 0 1]
-            \l [0 1 0 0]
-            \m [1 1]
-            \n [1 0]
-            \o [1 1 1]
-            \p [0 1 1 0]
-            \q [1 1 0 1]
-            \r [0 1 0]
-            \s [0 0 0]
-            \t [1]
-            \u [0 0 1]
-            \v [0 0 0 1]
-            \w [0 1 1]
-            \x [1 0 0 1]
-            \y [1 0 1 1]
-            \z [1 1 0 0]})
+(def letters {\a [0 1]
+              \b [1 0 0 0]
+              \c [1 0 1 0]
+              \d [1 0 0]
+              \e [0]
+              \f [0 0 1 0]
+              \g [1 1 0]
+              \h [0 0 0 0]
+              \i [0 0]
+              \j [0 1 1 1]
+              \k [1 0 1]
+              \l [0 1 0 0]
+              \m [1 1]
+              \n [1 0]
+              \o [1 1 1]
+              \p [0 1 1 0]
+              \q [1 1 0 1]
+              \r [0 1 0]
+              \s [0 0 0]
+              \t [1]
+              \u [0 0 1]
+              \v [0 0 0 1]
+              \w [0 1 1]
+              \x [1 0 0 1]
+              \y [1 0 1 1]
+              \z [1 1 0 0]})
 
 ;; time
 (def short-pulse 250)
@@ -60,14 +60,14 @@
 
 (comment "For the repl"
   (def board (arduino :firmata "/dev/ttyS42"))
-  (pin-mode board pin-number OUTPUT)
+  (pin-mode      board pin-number OUTPUT)
   (digital-write board pin-number HIGH)
   (digital-write board pin-number LOW))
 
-(defn morse-2-light "Given a word, make the led blink in morse (no upper case, no punctuation)"
+(defn morse "Given a word, make the led blink in morse (no upper case, no punctuation)"
   [board word]
   (doseq [l word]
-    (blink-letter board (morse l))))
+    (blink-letter board (letters l))))
 
 (defn main
   "Given a serial device entry, open the board, make it do some sos light and then close the board"
@@ -77,6 +77,6 @@
     (Thread/sleep 5000)
     (pin-mode board pin-number OUTPUT)
 
-    (morse-2-light "hello world")
+    (morse board "hello world")
 
     (close board)))
