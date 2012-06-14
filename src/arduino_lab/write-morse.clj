c20f9e99b0ded16b3102c973a10a00c2835317a9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-14 23:09:07 +0200

    Use a multimethod to make the led blink.

--- src/arduino_lab/write-morse.clj
+++ src/arduino_lab/write-morse.clj
@@ -18,31 +18,27 @@
 
 ;; functions
 
-(defn blink
-  "Given a board and time, make the led blink a given time"
+(defn blink "Given a board and time, make the led blink a given time"
   [board time]
   (digital-write board pin-led HIGH)
   (Thread/sleep time)
   (digital-write board pin-led LOW)
   (Thread/sleep time))
 
-(defn blink-letter
-  "Given a letter in morse representation, make the led blink accordingly (0 short pulse ; 1 long pulse)"
+(defmulti blink-letter-morse "Given a bit, blink the led accordingly in morse" identity)
+(defmethod blink-letter-morse 0 [_] (blink board short-pulse))
+(defmethod blink-letter-morse 1 [_] (blink board long-pulse))
+
+(defn blink-letter "Given a letter in morse representation, make the led blink accordingly (0 short pulse ; 1 long pulse)"
   [board letter-in-morse]
-  (doseq [i letter-in-morse]
-    (if (= i 0)
-      (blink board short-pulse)
-      (blink board long-pulse)))
+  (doseq [i letter-in-morse] (blink-letter-morse i))
   (Thread/sleep letter-delay))
 
-(defn write-morse
-  "Given a word, make the led blink in morse for each letter (no upper case, no punctuation)"
+(defn write-morse "Given a word, make the led blink in morse for each letter (no upper case, no punctuation)"
   [board word]
-  (doseq [l word]
-    (blink-letter board (m/letters-2-bits l))))
+  (doseq [l word] (blink-letter board (m/letters-2-bits l))))
 
-(comment
-  "For the repl - step by step"
+(comment "Execution code for the repl - step by step"
   (def device-board "/dev/ttyACM0")
   (System/setProperty "gnu.io.rxtx.SerialPorts" device-board)
   (def board (arduino :firmata device-board))
