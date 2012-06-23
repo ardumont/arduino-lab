d4ce2fc6c432dfbc5d59bbd0eea5bc80253dc5ce
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-23 14:24:49 +0200

    Remove useless main function.

--- src/arduino_lab/push-button.clj
+++ src/arduino_lab/push-button.clj
@@ -15,37 +15,10 @@
 (defn react-to-button
   "Given a board, read from the button"
   [board]
-  (println "read-morse" board)
   (while true
     (let [status-button (digital-read board pin-button)]
-      (println status-button)
       (digital-write board pin-led status-button)
       (Thread/sleep 10))))
-
-(defn main
-  "Given a serial device entry:
-   - open the board
-   - launch the circuit that permits the led to stay lightened as long as the button is pushed
-   - then close the board"
-  [board-serial-port]
-  (let [board (arduino :firmata board-serial-port)]
-
-    ;; allow arduino to boot
-    (Thread/sleep 5000)
-
-    ;; enable-pin for the button
-    (enable-pin board :digital pin-button)
-
-    ;; led in output mode
-    (pin-mode board pin-led OUTPUT)
-
-    ;; button in input mode
-    (pin-mode board pin-button INPUT)
-
-    ;; read the morse as we go
-    (react-to-button board)
-
-    (close board)))
 
 (comment
   "For the repl - step by step"
@@ -60,10 +33,4 @@
   (react-to-button board)
 
   (close board))
-
-(comment
-  "for the repl - one shot"
-  (def device-board "/dev/ttyACM0")
-  (System/setProperty "gnu.io.rxtx.SerialPorts" device-board)
-  (main device-board))
 
