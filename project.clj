a7e68b37198fa21afb94a1acefe469d0bd6ac968
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-23 14:04:24 +0200

    Migrate to lein2.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject arduino-lab "1.0.0-SNAPSHOT"
-  :description "Mess around with arduino from the confort of the repl"
+(defproject arduino-lab/arduino-lab "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clodiuno "0.0.3-SNAPSHOT"]
                  [serial-port "1.1.2"]
-                 [midje "1.3.1"]]
+                 [midje "1.4.0"]]
+  :profiles {:dev
+             {:dependencies
+              [[native-deps "1.0.5"]]}}
+  :min-lein-version "2.0.0"
   :native-dependencies [[org.clojars.samaaron/rxtx "2.2.0.1"]]
-  :dev-dependencies [[native-deps "1.0.5"]
-                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]]
-  :jvm-opts ["-Djava.library.path=./native/linux/x86/"
-             "-d32"])
+  :jvm-opts ["-Djava.library.path=./native/linux/x86/" "-d32"]
+  :description "Mess around with arduino from the confort of the repl")
 
