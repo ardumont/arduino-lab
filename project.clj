ae89636e205614ec09c114e229ea18f56c235399
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-06 19:41:30 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject arduino-lab/arduino-lab "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clodiuno "0.0.3-SNAPSHOT"]
+                 [clodiuno "0.0.4-SNAPSHOT"]
                  [serial-port "1.1.2"]
                  [midje "1.4.0"]]
   :profiles {:dev
@@ -10,4 +10,3 @@
   :native-dependencies [[org.clojars.samaaron/rxtx "2.2.0.1"]]
   :jvm-opts ["-Djava.library.path=./native/linux/x86/" "-d32"]
   :description "Mess around with arduino from the confort of the repl")
-
